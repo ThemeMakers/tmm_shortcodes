0e955575dcb936293fde86ca69c0f95118735b96
Dmytro Tkachenko <tkachenkodimitriy@gmail.com>
2016-08-11 19:51:15 +0300

    Google Map update.

--- views/default/google_map.php
+++ views/default/google_map.php
@@ -1,9 +1,9 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed');
 $inique_id = uniqid();
-$google_maps_api_key = (isset($key)) ? 'key=' . $key . '&' : '' ;
-$map_link = '//maps.google.com/maps/api/js?' . $google_maps_api_key . 'sensor=false';
+$google_maps_api_key = (isset($key)) ? 'key=' . $key : '' ;
+$map_link = '//maps.google.com/maps/api/js?' . $google_maps_api_key . '&sensor=false';
 wp_enqueue_script('tmm_google_maps_api', $map_link);
-wp_enqueue_script('tmm_google_maps_api', 'http://maps.google.com/maps/api/js?sensor=false');
+wp_enqueue_script('tmm_google_maps_api', '//maps.google.com/maps/api/js?' . $google_maps_api_key . '&sensor=false');
 wp_enqueue_script('tmm_theme_markerwithlabel_js', TMM_Ext_Shortcodes::get_application_uri() . '/js/shortcodes/markerwithlabel.js', array(), false, true);
 wp_enqueue_script("tmm_shortcode_google_map_js", TMM_Ext_Shortcodes::get_application_uri() . '/js/shortcodes/google_map.js', array(), false, true);
 
@@ -32,7 +32,7 @@ if (isset($location_mode)) {
 	if ($location_mode == 'address') {
 		$address = str_replace(' ', '+', $address);
 
-		$geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false');
+		$geocode = file_get_contents('//maps.google.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false');
 		if(isset($geocode) && $geocode){
 			$output = json_decode($geocode);
 		}
@@ -54,9 +54,21 @@ if (!isset($maptype)) {
 
 	<div class="google_map" id="google_map_<?php echo $inique_id ?>" style="width:100%;height: <?php echo $height ?>px;"></div>
 
-	<script type="text/javascript">
+	<script async type="text/javascript">
 		jQuery(function() {
-			gmt_init_map(<?php echo $latitude ?>,<?php echo $longitude ?>, "google_map_<?php echo $inique_id ?>", <?php echo (int) $zoom ?>, "<?php echo $maptype ?>", "<?php echo $content ?>", "<?php echo $enable_marker ?>", "<?php echo $enable_popup ?>", "<?php echo $enable_scrollwheel ?>",<?php echo $js_controls ?>, "<?php echo @$marker_is_draggable ?>");
+			gmt_init_map(
+				<?php echo $latitude ?>,
+				<?php echo $longitude ?>,
+				"google_map_<?php echo $inique_id ?>",
+				<?php echo (int) $zoom ?>,
+                "<?php echo $maptype ?>",
+                "<?php echo $content ?>",
+                "<?php echo $enable_marker ?>",
+                "<?php echo $enable_popup ?>",
+                "<?php echo $enable_scrollwheel ?>",
+                <?php echo $js_controls ?>,
+                "<?php echo @$marker_is_draggable ?>"
+            );
 		});
 	</script>
 <?php else: ?>
@@ -67,8 +79,9 @@ if (!isset($maptype)) {
 	}
 
 	$location_mode_string = 'center=' . $latitude . ',' . $longitude;
+
 	?>
 
-	<img src="http://maps.googleapis.com/maps/api/staticmap?<?php echo $location_mode_string ?>&zoom=<?php echo (int) $zoom ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo $width ?>x<?php echo $height ?><?php echo $marker_string ?>&sensor=false">
+	<img src="https://maps.googleapis.com/maps/api/staticmap?<?php echo $location_mode_string ?>&zoom=<?php echo (int) $zoom ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo (int)$width ?>x<?php echo (int)$height ?><?php echo $marker_string ?>&<?php echo $google_maps_api_key ?>">
 
 <?php endif; ?>
