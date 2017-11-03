6523412687468aa3dc4c2dfa67f580900682afe1
Oleksiy Ostapenko <lelik4w@gmail.com>
2017-11-03 22:21:10 +0100

    fix for captcha appearing when child theme activated

--- views/accio/contact_form.php
+++ views/accio/contact_form.php
@@ -84,7 +84,7 @@ if (!empty($contact_form['inputs'])) {
 
 			<p class="input-block">
 				<?php $hash = md5(time()); ?>
-				<img class="contact_form_capcha" src="<?php echo get_stylesheet_directory_uri(); ?>/helper/capcha/image.php?hash=<?php echo $hash ?>" height="27" width="72" />
+				<img class="contact_form_capcha" src="<?php echo get_template_directory_uri(); ?>/helper/capcha/image.php?hash=<?php echo $hash ?>" height="27" width="72" />
 				<input type="text" value="" name="verify" class="verify" />
 				<input type="hidden" name="verify_code" value="<?php echo $hash ?>" />
 			</p><!--/ .row-->
