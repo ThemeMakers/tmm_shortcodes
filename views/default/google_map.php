de9e3c53ff9cef62cf5cef1f7e3f37f26dd3a976
Oleksiy Ostapenko <lelik4w@gmail.com>
2018-11-13 01:28:43 +0100

    update google api urls

--- views/default/google_map.php
+++ views/default/google_map.php
@@ -5,11 +5,11 @@ if (TMM::get_option("api_key_google")){
 $inique_id = uniqid();
 
 $google_maps_api_key = (TMM::get_option("api_key_google")) ? 'key=' . TMM::get_option("api_key_google") : '' ;
-$map_link = '//maps.google.com/maps/api/js?' . $google_maps_api_key . '&sensor=false';
+$map_link = 'https://maps.google.com/maps/api/js?' . $google_maps_api_key;
 
 
 	wp_enqueue_script('tmm_google_maps_api', $map_link);
-	wp_enqueue_script('tmm_google_maps_api', '//maps.google.com/maps/api/js?' . $google_maps_api_key . '&sensor=false');
+	wp_enqueue_script('tmm_google_maps_api', 'https://maps.google.com/maps/api/js?' . $google_maps_api_key);
 	wp_enqueue_script('tmm_theme_markerwithlabel_js', TMM_Ext_Shortcodes::get_application_uri() . '/js/shortcodes/markerwithlabel.js', array(), false, true);
 	wp_enqueue_script("tmm_shortcode_google_map_js", TMM_Ext_Shortcodes::get_application_uri() . '/js/shortcodes/google_map.js', array(), false, true);
 
@@ -39,7 +39,7 @@ if (isset($location_mode)) {
 	if ($location_mode == 'address') {
 		$address = str_replace(' ', '+', $address);
 
-		$geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false');
+		$geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . urlencode($address));
 		if(isset($geocode) && $geocode){
 			$output = json_decode($geocode);
 		}
@@ -89,7 +89,7 @@ if (!isset($maptype)) {
 
 	?>
 
-	<img src="https://maps.googleapis.com/maps/api/staticmap?<?php echo $location_mode_string ?>&zoom=<?php echo (int) $zoom ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo (int)$width ?>x<?php echo (int)$height ?><?php echo $marker_string ?>&key=<?php echo TMM::get_option("api_key_google")?>&sensor=false">
+	<img src="https://maps.google.com/maps/api/staticmap?<?php echo $location_mode_string ?>&zoom=<?php echo (int) $zoom ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo (int)$width ?>x<?php echo (int)$height ?><?php echo $marker_string ?>&key=<?php echo TMM::get_option("api_key_google")?>">
 
 <?php endif;
 
